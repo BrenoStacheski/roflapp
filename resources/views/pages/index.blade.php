89ac5706c96898bd84fbf16334ca4bfac75f33ab
Breno Stacheski <brenosttc@gmail.com>
2022-06-27 21:54:38 -0300

    alteração de index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div class="jumbotron text-center">
-    <h1>{{$title}}</h1>
-        <p>Esse é o mais novo aplicativo para você guardar suas risadas diárias!</p>
+    <h1>{{Breno Stacheski}}</h1>
+        <p>Sistema criado para visualização de currículo profissional.</p>
         <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Registrar-se</a></p>
     </div>
 @endsection
