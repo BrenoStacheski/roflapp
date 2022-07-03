9d04ee80d0e964679bb3ec04094b09ba770b5c29
Breno Stacheski <brenosttc@gmail.com>
2022-07-03 12:22:25 -0300

    alteração .env e index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,6 +4,5 @@
     <div class="jumbotron text-center">
     <h1>Breno Stacheski</h1>
         <p>Sistema criado para visualização de currículo profissional.</p>
-        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Registrar-se</a></p>
     </div>
 @endsection
